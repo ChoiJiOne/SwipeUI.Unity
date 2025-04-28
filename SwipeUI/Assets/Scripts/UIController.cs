9364489aedb302f57cf4c0091d7181ac47760a2d
CHOIJIONE\choij <choijione02@gmail.com>
2025-04-29 08:12:27 +0900

    버튼으로 배경 선택 UI를 제어하는 기능 추가

--- SwipeUI/Assets/Scripts/UIController.cs
+++ SwipeUI/Assets/Scripts/UIController.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    [SerializeField] private Button _activeButton;
+    [SerializeField] private Button _deactiveButton;
+    [SerializeField] private GameObject _bgSelector;
 
+    private void Awake()
+    {
+        _activeButton.onClick.AddListener(OnActiveButtonClicked);
+        _deactiveButton.onClick.AddListener(OnDeactiveButtonClicked);
+    }
 
+    private void OnActiveButtonClicked()
+    {
+        _bgSelector.SetActive(true);
+    }
 
+    private void OnDeactiveButtonClicked()
+    {
+        _bgSelector.SetActive(false);
+    }
 }
