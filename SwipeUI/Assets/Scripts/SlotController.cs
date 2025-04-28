763d236fef97eee9a6c8f68e9f7d52440055a3c4
CHOIJIONE\choij <choijione02@gmail.com>
2025-04-29 07:44:59 +0900

    개수가 정해진 슬롯을 선택하도록 변경

--- SwipeUI/Assets/Scripts/SlotController.cs
+++ SwipeUI/Assets/Scripts/SlotController.cs
@@ -1,28 +1,11 @@
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class SlotController : MonoBehaviour
 {
-    public UnityAction<SlotController> OnDestroySlot;
     public float Width => _rectTransform.rect.width;
+    public Sprite Sprite => _image.sprite;
     
     [SerializeField] private RectTransform _rectTransform;
-    [SerializeField] private Button _okButton;
-    [SerializeField] private Image _bgImage;
-    [SerializeField] private Sprite[] _bgSprites;
-
-    private void Awake()
-    {
-        _okButton.onClick.AddListener(OnOkButtonClicked);
-
-        int randomIndex = Random.Range(0, _bgSprites.Length);
-        _bgImage.sprite = _bgSprites[randomIndex];
-    }
-
-    private void OnOkButtonClicked()
-    {
-        OnDestroySlot?.Invoke(this);
-        Destroy(gameObject);
-    }
+    [SerializeField] private Image _image;
 }
--- SwipeUI/Assets/Scripts/SwipeUI.cs
+++ SwipeUI/Assets/Scripts/SwipeUI.cs
@@ -9,25 +9,31 @@ public class SwipeUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragH
 {
     [Header("UI")]
     [SerializeField] private TextMeshProUGUI _slotIndex;
-    [SerializeField] private Button _createButton;
+    [SerializeField] private Button _selectButton;
+    [SerializeField] private Image _bgImage;
     [SerializeField] private RectTransform _contentRect;
 
     [Header("Swipe")]
-    [SerializeField] private SlotController _slotControllerPrefab;
+    [SerializeField] private List<SlotController> _slotControllers = new();
     [SerializeField] private Scrollbar _scrollBar;
     [SerializeField] private float _swipeThreshold;
     [SerializeField] private float _swipeTime;
 
-    private List<SlotController> _slotControllers = new();
     private float _totalWidth = 0.0f;
     private float _startPosX = 0.0f;
     private float _endPosX = 0.0f;
-    private int _currentSlotIndex = -1;
+    private int _currentSlotIndex = 0;
     private Tween _scrollTween;
 
     private void Awake()
     {
-        _createButton.onClick.AddListener(OnCreateButtonClicked);
+        foreach (var slotController in _slotControllers)
+        {
+            _totalWidth += slotController.Width;
+        }
+
+        _slotIndex.text = $"{_currentSlotIndex + 1} / {_slotControllers.Count}";
+        _selectButton.onClick.AddListener(OnSelectButtonClicked);
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -41,7 +47,7 @@ public class SwipeUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragH
         float dragDelta = eventData.position.x - _startPosX;
         float normalizedDelta = dragDelta / _totalWidth;
 
-        float baseValue = _slotControllers.Count <= 1 ? 0f : (float)(_currentSlotIndex) / (_slotControllers.Count - 1);
+        float baseValue = (float)(_currentSlotIndex) / (_slotControllers.Count - 1);
         float newValue = Mathf.Clamp01(baseValue - normalizedDelta);
 
         _scrollBar.value = newValue;
@@ -67,7 +73,7 @@ public class SwipeUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragH
         _currentSlotIndex = Mathf.Clamp(_currentSlotIndex, 0, _slotControllers.Count - 1);
         _slotIndex.text = $"{_currentSlotIndex + 1} / {_slotControllers.Count}";
 
-        float targetValue = _slotControllers.Count <= 1 ? 0.0f : (float)(_currentSlotIndex) / (_slotControllers.Count - 1);
+        float targetValue = (float)(_currentSlotIndex) / (_slotControllers.Count - 1);
         _scrollTween = DOTween.To(GetScrollBarValue, SetScrollBarValue, targetValue, _swipeTime).SetEase(Ease.OutQuad);
     }
 
@@ -77,42 +83,8 @@ public class SwipeUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragH
         _scrollBar.value = value;
     }
 
-    private void OnCreateSlot(SlotController newSlotController)
+    private void OnSelectButtonClicked()
     {
-        if (_currentSlotIndex == -1)
-        {
-            _currentSlotIndex = 0;
-        }
-
-        newSlotController.OnDestroySlot += OnDestroySlot;
-
-        _totalWidth += newSlotController.Width;
-        _slotControllers.Add(newSlotController);
-        _slotIndex.text = $"{_currentSlotIndex + 1} / {_slotControllers.Count}";
-    }
-
-    private void OnDestroySlot(SlotController slotController)
-    {
-        _totalWidth -= slotController.Width;
-        _slotControllers.Remove(slotController);
-
-        slotController.OnDestroySlot -= OnDestroySlot;
-
-        if (_slotControllers.Count == 0)
-        {
-            _currentSlotIndex = -1;
-        }
-        else
-        {
-            _currentSlotIndex = Mathf.Clamp(_currentSlotIndex, 0, _slotControllers.Count - 1);
-        }
-
-        _slotIndex.text = $"{_currentSlotIndex + 1} / {_slotControllers.Count}";
-    }
-
-    private void OnCreateButtonClicked()
-    {
-        SlotController newSlotController = Instantiate(_slotControllerPrefab, _contentRect);
-        OnCreateSlot(newSlotController);
+        _bgImage.sprite = _slotControllers[_currentSlotIndex].Sprite;
     }
 }
